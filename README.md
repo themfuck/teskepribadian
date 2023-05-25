9956260e511b027de105d732a312eb264fe47739
much ramdan <muchramdan123@gmail.com>
2023-05-25 20:26:45 +0700

    mother

---
+++ README.md
@@ -0,0 +1,15 @@
+# AnimeSwap Interface
+
+An open source interface for AnimeSwap -- a protocol for decentralized exchange on Aptos.
+
+- Website: [suiofhanako.xyz](https://suiofhanako.xyz)
+- Interface: [app.suiofhanako.xyz](https://app.suiofhanako.xyz)
+- Docs: [docs.suiofhanako.xyz](https://docs.suiofhanako.xyz)
+- Twitter: [@animeswap_org](https://twitter.com/animeswap_org)
+- Email: [support@suiofhanako.xyz](mailto:support@suiofhanako.xyz)
+- Discord: [AnimeSwap](https://discord.gg/rbUG6SpRAM)
+- Whitepapers:
+  - [v1](https://docs.suiofhanako.xyz/blog/)
+## Accessing the AnimeSwap Interface
+
+To access the AnimeSwap Interface, visit [app.suiofhanako.xyz](https://app.suiofhanako.xyz).
---
+++ src/pages/App.tsx
@@ -0,0 +1,151 @@
+import Footer from 'components/Footer'
+import Loader from 'components/Loader'
+import TopLevelModals from 'components/TopLevelModals'
+import { useFeatureFlagsIsLoaded } from 'featureFlags'
+import { Suspense, useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import styled from 'styled-components/macro'
+
+import { useAnalyticsReporter } from '../components/analytics'
+import ErrorBoundary from '../components/ErrorBoundary'
+import Header from '../components/Header'
+import Popups from '../components/Popups'
+import DarkModeQueryParamReader from '../theme/DarkModeQueryParamReader'
+import AddLiquidity from './AddLiquidity'
+import { RedirectDuplicateTokenIds } from './AddLiquidity/redirects'
+import Chart from './Chart'
+import Pool from './Pool'
+import RemoveLiquidity from './RemoveLiquidity'
+import Swap from './Swap'
+import { RedirectPathToSwapOnly, RedirectToSwap } from './Swap/redirects'
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: flex-start;
+  min-height: 100vh;
+`
+
+const BodyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 120px 16px 0px 16px;
+  align-items: center;
+  flex: 1;
+  z-index: 1;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    padding: 4rem 8px 16px 8px;
+  `};
+`
+
+const HeaderWrapper = styled.div`
+  ${({ theme }) => theme.flexRowNoWrap}
+  width: 100%;
+  justify-content: space-between;
+  position: fixed;
+  top: 0;
+  z-index: 2;
+`
+
+const FooterWrapper = styled.div`
+  ${({ theme }) => theme.flexRowNoWrap}
+  width: 100%;
+  justify-content: center;
+  position: flex;
+  padding: 60px 0px 100px 0px;
+`
+
+const BottomRightLogo = styled.div`
+  background: url('images/background.jpg');
+  width: 220px;
+  height: 220px;
+  position: fixed;
+  right: 0px;
+  bottom: 0px;
+  -webkit-transform: scaleX(-1);
+  transform: scaleX(-1);
+
+  ${({ theme }) => theme.mediaWidth.upToMedium`
+    scale: 0.8;
+    right: -22px;
+    bottom: -22px;
+  `};
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    scale: 0.5;
+    right: -56px;
+    bottom: -54px;
+  `};
+`
+
+const BottomLeftLogo = styled.div`
+  background: url('images/left_char.png');
+  width: 220px;
+  height: 220px;
+  position: fixed;
+  left: 0px;
+  bottom: 0px;
+
+  ${({ theme }) => theme.mediaWidth.upToMedium`
+    scale: 0.8;
+    left: -22px;
+    bottom: -22px;
+  `};
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    scale: 0.5;
+    left: -56px;
+    bottom: -54px;
+  `};
+`
+
+export default function App() {
+  const isLoaded = useFeatureFlagsIsLoaded()
+  const { pathname } = useLocation()
+
+  useAnalyticsReporter()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return (
+    <ErrorBoundary>
+      <DarkModeQueryParamReader />
+      <AppWrapper>
+        <HeaderWrapper>
+          <Header />
+        </HeaderWrapper>
+        <BodyWrapper>
+          <Popups />
+          <TopLevelModals />
+          <Suspense fallback={<Loader />}>
+            {isLoaded ? (
+              <Routes>
+                <Route path="swap/:toCoin" element={<RedirectToSwap />} />
+                <Route path="swap" element={<Swap />} />
+                {/* <Route path="pool" element={<Pool />} /> */}
+                <Route path="add" element={<RedirectDuplicateTokenIds />}>
+                  <Route path=":coinIdA" element={<AddLiquidity />} />
+                  <Route path=":coinIdA/:coinIdB" element={<AddLiquidity />} />
+                </Route>
+                <Route path="remove/:coinIdA/:coinIdB" element={<RemoveLiquidity />} />
+                <Route path="charts" element={<Chart />} />
+                <Route path="*" element={<RedirectPathToSwapOnly />} />
+              </Routes>
+            ) : (
+              <Loader />
+            )}
+          </Suspense>
+        </BodyWrapper>
+        <FooterWrapper>
+          <Footer />
+        </FooterWrapper>
+        {/* <BottomRightLogo /> */}
+        {/* <BottomLeftLogo /> */}
+      </AppWrapper>
+    </ErrorBoundary>
+  )
+}
